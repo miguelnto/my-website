90ee1bf60bbe4c998e0235ad693bd424e397858d
Miguel <miguelup01@outlook.com>
2024-10-02 00:00:56 -0300

    Updated site

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,7 @@
 
 <br>
 
-<p>At the moment I'm a Game Developer searching for new opportunities. I love to learn new technologies and use them in various projects, always trying to improve my developer experience. In my blog, you'll find tutorials, tips and opinions on technology-related topics. You can find me on a lot of websites, take a look at the <a href="/contact">contact</a> page.</p>
+<p>At the moment I'm a Freelance Developer searching for new opportunities. I love to learn about new technologies and use them in various projects where they make sense, and am always trying to improve my developer experience. In my blog, you'll find tutorials, tips and opinions on technology-related topics. You can find me on a lot of websites, take a look at the <a href="/contact">contact</a> page.</p>
 
 <br>
 
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,6 +9,5 @@
 <ul>
 	<li><b>- Email:</b> miguelup01@outlook.com</li>
 	<li><b>- Github:</b> <a href='https://github.com/miguelnto'>miguelnto</a></li>
-	<li><b>- LinkedIn:</b> <a href='https://www.linkedin.com/in/miguelbarrosneto'> miguelbarrosneto</a></li>
 	<li><b>- Discord:</b> miguelnto</li>
 </ul>
